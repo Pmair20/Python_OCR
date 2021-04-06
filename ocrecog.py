2f3d3d65126b62563c2f6692a5f05fde2e977bb6
Pritish Mair <59889857+Pmair20@users.noreply.github.com>
2021-04-06 13:09:30 +0530

    Update ocrecog.py

--- ocrecog.py
+++ ocrecog.py
@@ -1,8 +1,8 @@
 import cv2
 import pytesseract
 import numpy as np
-pytesseract.pytesseract.tesseract_cmd = r'D:\Python_Codes/tesseract.exe'       # path to the tesseract exe after installing from the link mentioned in setup_link txt
-img = cv2.imread('D:\Python_Codes/trial.png')                                  # path to the text image to use OCR
+pytesseract.pytesseract.tesseract_cmd = r'<path to the tesseract exe>'       # path to the tesseract exe after installing from the link mentioned in setup_link txt
+img = cv2.imread('<path to the image to be used for OCR>')                                  # path to the text image to use OCR
 gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)                                    # converting to gray scale(black and white) for better recognition
 gray, img_bin = cv2.threshold(gray,128,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 gray = cv2.bitwise_not(img_bin)
